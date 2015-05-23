20df85c4eb6188034bb89a3acb2a26571d0db705
Curtis Schlak <realistschuckle@gmail.com>
2015-05-23 12:59:33 -0500

    Prevent reallocation of memory.

--- ascii-program.js
+++ ascii-program.js
@@ -4,9 +4,16 @@ var play = document.getElementById('play');
 
 var program = new chip8x.Program();
 program.clearScreen();
+program.setCharInto(0x0, 0);
+for (var i = 0; i < 0x10; i += 1) {
+  program.draw(0, 1, i * 4, 7, 1);
+}
 for (var i = 0; i < 0x10; i += 1) {
   program.setCharInto(i, 0);
   program.draw(0, 1, 2 + (i * 8) % 64, 9 + Math.floor(i / 8) * 8, 5);
+}
+for (var i = 0; i < 0x10; i += 1) {
+  program.draw(0, 1, i * 4, 23, 1);
 }
 program.end();
 
@@ -20,18 +27,22 @@ play.addEventListener('click', function () {
   window.setTimeout(updateScreen, 0);
 });
 
+var texts = new Array(0x20);
+for (var i = 0x00; i < 0x20; i += 1) {
+  texts[i] = new Array(0x40);
+}
+var rowText = new Array(0x20);
 function updateScreen() {
   if (emulator.dirty) {
     var gfx = emulator.gfx;
-    var text = [];
     for (var y = 0; y < 32; y += 1) {
-      var row = [];
       for (var x = 0; x < 64; x += 1) {
-        row.push(gfx[y * 64 + x] > 0? '*' : ' ');
+        texts[y][x] = gfx[y * 64 + x] > 0? '*' : ' ';
       }
-      text.push(row.join(''));
+      rowText[y] = texts[y].join('');
     }
-    screen.innerHTML = text.join('\n');
+
+    screen.innerHTML = rowText.join('\n');
   }
   if (emulator.running) {
     window.setTimeout(updateScreen, 0);
